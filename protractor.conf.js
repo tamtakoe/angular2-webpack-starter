633443cb707ded29609ded674e61c97fdc5fa57d
tamtakoe <tamtakoe@gmail.com>
2017-08-22 14:09:09 +0300

    bug demo

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,32 +1,8 @@
-// @AngularClass
+/**
+ * @author: @AngularClass
+ */
 
-exports.config = {
-  baseUrl: 'http://localhost:8080/',
-
-  allScriptsTimeout: 11000,
-
-  framework: 'jasmine',
-
-  jasmineNodeOpts: {
-    defaultTimeoutInterval: 60000,
-    showTiming: true
-  },
-
-  capabilities: {
-    'browserName': 'chrome',
-    'chromeOptions': {
-      'args': ['show-fps-counter=true']
-    }
-  },
-
-  seleniumAddress: 'http://localhost:4444/wd/hub',
-
-  specs: [
-    'test/**/*.e2e.js'
-  ],
-
-  onPrepare: function() {
-    browser.ignoreSynchronization = true;
-  }
-
-};
+/**
+ * look in ./config for protractor.conf.js
+ */
+exports.config = require('./config/protractor.conf.js').config;
